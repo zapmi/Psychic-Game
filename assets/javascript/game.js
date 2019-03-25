db2c421858e5375076c2efa71819e087318295f2
zapmi <mzapata2991@gmail.com>
2019-03-24 23:30:20 -0500

    got some stuff working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,71 +1,87 @@
-var car = {
-    make: "Honda",
-    model: "Fit",
-    color: "Blue Raspberry",
-    mileage: 3000,
-    isWorking: true,
+var martrixChars = ["Morpheus", "Trinity", "Agent Smith"];
+  
+  var randomChar = martrixChars[Math.floor(Math.random()*martrixChars.length)];
+  
+  document.body.innerHTML = randomChar;
 
-    driveToWork: function () {
+// // Loop through the array we just set up.
+// for (var i = 0; i < nums.length; i++) {
+//     // If the current number is greater than 2..
+//     if (nums[i] > 2) {
+//       // Log "number is greater than 2".
+//       console.log(nums[i] + " is greater than 2");
+//     }
 
-        console.log("Old Mileage: " + this.mileage);
+    var car = {
+        make: "Honda",
+        model: "Fit",
+        color: "Blue Raspberry",
+        mileage: 3000,
+        isWorking: true,
 
-        this.mileage = this.mileage + 8;
+        driveToWork: function () {
 
-        console.log("New mileage: " + this.mileage);
-    },
+            console.log("Old Mileage: " + this.mileage);
 
-    driveAroundWorld: function () {
+            this.mileage = this.mileage + 8;
 
-        console.log("Old Mileage: " + this.mileage);
+            console.log("New mileage: " + this.mileage);
+        },
 
-        this.mileage = this.mileage + 24000;
+        driveAroundWorld: function () {
 
-        console.log("New Mileage: " + this.mileage);
-        console.log("Car needs a tuneup!");
+            console.log("Old Mileage: " + this.mileage);
 
-        this.isWorking = false;
-    },
+            this.mileage = this.mileage + 24000;
 
-    getTuneUp: function () {
-        console.log("Car is ready to go!");
-        this.isWorking = true;
-    },
+            console.log("New Mileage: " + this.mileage);
+            console.log("Car needs a tuneup!");
 
-    honk: function () {
-        console.log("Honk! Honk!");
-    }
-}
+            this.isWorking = false;
+        },
 
-function writeCarStats() {
-    console.log("Make: " + car.make);
-    console.log("Model: " + car.model);
-    console.log("Color: " + car.color);
-    console.log("Milage: " + car.mileage);
-    console.log("Car works: " + car.isWorking);
-    // console.log("Make: " + car.make);
-}
+        getTuneUp: function () {
+            console.log("Car is ready to go!");
+            this.isWorking = true;
+        },
 
-document.onkeyup = function (event) {
-    var userKeys = event.key;
-
-
-    if (userKeys === "h") {
-        car.honk();
-        writeCarStats();
-    } else if (userKeys === "d") {
-        car.driveToWork();
-        writeCarStats();
-    } else if (userKeys === "w") {
-        car.driveAroundWorld();
-        writeCarStats();
-    } else if (userKeys === "t") {
-        car.getTuneUp();
-        writeCarStats();
+        honk: function () {
+            console.log("Honk! Honk!");
+        }
     }
 
+    function writeCarStats() {
+        console.log("Make: " + car.make);
+        console.log("Model: " + car.model);
+        console.log("Color: " + car.color);
+        console.log("Milage: " + car.mileage);
+        console.log("Car works: " + car.isWorking);
+        // console.log("Make: " + car.make);
+    }
+
+    document.onkeyup = function (event) {
+        var userKeys = event.key;
 
 
-    // if (userKeys === "h") {
-    //   console.log(car.reWriteStats())
-    // }
-}
+        if (userKeys === "h") {
+            car.honk();
+            writeCarStats();
+            document.getElementById("demo").innerHTML = "h";
+        } if (userKeys === "d") {
+            car.driveToWork();
+            writeCarStats();
+            document.getElementById("demo").innerHTML = "d";
+        } else if (userKeys === "w") {
+            car.driveAroundWorld();
+            writeCarStats();
+        } else if (userKeys === "t") {
+            car.getTuneUp();
+            writeCarStats();
+        }
+
+
+
+        // if (userKeys === "h") {
+        //   console.log(car.reWriteStats())
+        // }
+    }
