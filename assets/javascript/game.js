4a96f487e7e20139c97fe0175470c7f056022c93
zapmi <mzapata2991@gmail.com>
2019-03-25 19:29:15 -0500

    deleted stuff that was not needed, added a couple loops

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,87 +1,52 @@
-var martrixChars = ["Morpheus", "Trinity", "Agent Smith"];
-  
-  var randomChar = martrixChars[Math.floor(Math.random()*martrixChars.length)];
-  
-  document.body.innerHTML = randomChar;
+// array with characters to choose from
+var mortKomChar = ["Morpheus", "Trinity", "Agent Smith"];
 
+// this creates a var and picks a random name from mortKomChar array
+var randomChar = mortKomChar[Math.floor(Math.random() * mortKomChar.length)];
+
+//   document.body.innerHTML = randomChar;
+// creates an array for the char chosen and replaces with dashes
+var pickdChar = [];
 // // Loop through the array we just set up.
-// for (var i = 0; i < nums.length; i++) {
-//     // If the current number is greater than 2..
-//     if (nums[i] > 2) {
-//       // Log "number is greater than 2".
-//       console.log(nums[i] + " is greater than 2");
-//     }
+for (var i = 0; i < randomChar.length; i++) {
+} pickdChar[i] = "-"
 
-    var car = {
-        make: "Honda",
-        model: "Fit",
-        color: "Blue Raspberry",
-        mileage: 3000,
-        isWorking: true,
+//var holds remaining letters
+var reaminingLetters = randomChar.length;
 
-        driveToWork: function () {
+var attempts = 11;
 
-            console.log("Old Mileage: " + this.mileage);
-
-            this.mileage = this.mileage + 8;
-
-            console.log("New mileage: " + this.mileage);
-        },
-
-        driveAroundWorld: function () {
-
-            console.log("Old Mileage: " + this.mileage);
-
-            this.mileage = this.mileage + 24000;
-
-            console.log("New Mileage: " + this.mileage);
-            console.log("Car needs a tuneup!");
-
-            this.isWorking = false;
-        },
-
-        getTuneUp: function () {
-            console.log("Car is ready to go!");
-            this.isWorking = true;
-        },
-
-        honk: function () {
-            console.log("Honk! Honk!");
-        }
-    }
-
-    function writeCarStats() {
-        console.log("Make: " + car.make);
-        console.log("Model: " + car.model);
-        console.log("Color: " + car.color);
-        console.log("Milage: " + car.mileage);
-        console.log("Car works: " + car.isWorking);
-        // console.log("Make: " + car.make);
-    }
-
-    document.onkeyup = function (event) {
-        var userKeys = event.key;
+for (var i = 11; i < attempts.length; i--){
+    
+}
+if (attempts == 0) {
+    alert("GAME OVER");
+}
 
 
-        if (userKeys === "h") {
-            car.honk();
-            writeCarStats();
-            document.getElementById("demo").innerHTML = "h";
-        } if (userKeys === "d") {
-            car.driveToWork();
-            writeCarStats();
-            document.getElementById("demo").innerHTML = "d";
-        } else if (userKeys === "w") {
-            car.driveAroundWorld();
-            writeCarStats();
-        } else if (userKeys === "t") {
-            car.getTuneUp();
-            writeCarStats();
-        }
+
+    // document.onkeyup = function (event) {
+    //     var userKeys = event.key;
+
+
+    //     if (userKeys === "h") {
+    //         car.honk();
+    //         writeCarStats();
+    //         document.getElementById("demo").innerHTML = "h";
+    //     } if (userKeys === "d") {
+    //         car.driveToWork();
+    //         writeCarStats();
+    //         document.getElementById("demo").innerHTML = "d";
+    //     } else if (userKeys === "w") {
+    //         car.driveAroundWorld();
+    //         writeCarStats();
+    //     } else if (userKeys === "t") {
+    //         car.getTuneUp();
+    //         writeCarStats();
+    //     }
 
 
 
         // if (userKeys === "h") {
         //   console.log(car.reWriteStats())
         // }
-    }
