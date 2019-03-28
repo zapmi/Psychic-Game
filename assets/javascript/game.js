cd88a83df7cf1eee71c6e0e057d21b9a0925c9bf
zapmi <mzapata2991@gmail.com>
2019-03-27 21:02:31 -0500

    took it back a few steps

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,16 @@
 window.onload = function () {
 
     // Creates an array that lists out all of the options.
-    var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    
     var wins = 0;
     var losses = 0;
     var attempts = 10;
-    var currAttempts = "";
-    // var yourGuess = [];
+    // var currAttempts = "";
+    var yourGuess = [];
+    var computerChoices = "Thequickbrownfxjmpsvlazydg";
     // Randomly chooses a letter from the choices array. This is the Computer's guess.
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-    var randLett = computerGuess;
+    // var randLett = computerGuess;
     // This function is run whenever the user presses a key.
     // document.onkeyup = function (event) {
 
@@ -21,79 +22,107 @@ window.onload = function () {
     document.getElementById("wins").innerHTML = wins;
     document.getElementById("loss").innerHTML = losses;
     document.getElementById("guessLeft").innerHTML = attempts;
-    document.getElementById("guessSoFar").innerHTML = currAttempts;
+    // document.getElementById("guessSoFar").innerHTML = currAttempts;
 
 
     // document.addEventListener("keydown", function (event) {
     //     var input = array.computerChoices(event.keyCode);
 
-    // checkLetter();
+    checkLetter();
 
-    // function checkLetter() {
+    function checkLetter() {
 
-    // document.getElementById("inputBox").addEventListener("keydown", myFunction);
+        // document.getElementById("inputBox").addEventListener("keydown", myFunction);
 
-    // function myFunction() {
-    //     // document.getElementById("inputBox").style.backgroundColor = "red";
-    //     // var userChoice = String.fromCharCode(event.keyCode).toLowerCase();
+        // function myFunction() {
+        //     // document.getElementById("inputBox").style.backgroundColor = "red";
+        //     // var userChoice = String.fromCharCode(event.keyCode).toLowerCase();
 
-    //     if (yourGuess === randLett) {
-    //         attempts = 10;
-    //         // randLett = computerGuess;
-    //         currAttempts = "";
-    //         wins++;
-    //     }
+        //     if (yourGuess === randLett) {
+        //         attempts = 10;
+        //         // randLett = computerGuess;
+        //         currAttempts = "";
+        //         wins++;
+        //     }
 
-    //     else {
+        //     else {
 
-    document.onkeypress = function (event) {
-        var yourGuess = event.key;
+        document.onkeyup = function (event) {
+            var yourGuess = event.key;
 
-        if (yourGuess === randLett) {
-            wins++;
-        } else {
-            attempts--;
-            guessLeft--;
+            if (yourGuess === computerGuess) {
+                wins++;
+                attempts = 10;
+                yourGuess = [];
+                // } else {
+                //     attempts--;
+                //     // guessLeft--;
+                // }
+
+                // if (attempts === 0) {
+                //     losses++
+
+                if (yourGuess !== computerGuess) {
+                    attempts--;
+
+                }
+
+                //when remaining attempts equals zero, lost is incrimented by 1; attempts is reset to 10, and used letters array is cleared
+                if (attempts == 0) {
+                    lost++;
+                    yourGuess = []
+                    attempts = 10;
+                }
+
+                //HANDELING INCORRECT GUESSES - OUTPUT
+                //this 'if' prevents a letter selected a 2nd time from being written to the usedArray again, although it still counts as a guess
+                if (yourGuess.indexOf(pGuess) >= 0) {
+
+                } else {
+                    //this pushes the players incorrect guess to the usedArray and writes it to the HTML
+                    yourGuess.push(pGuess);
+                    document.getElementById('pGuess').innerHTML = yourGuess;
+                    console.log(yourGuess);
+
+                }
+
+
+
+
+                document.getElementById("wins").innerHTML = wins;
+                document.getElementById("loss").innerHTML = losses;
+                document.getElementById("guessLeft").innerHTML = attempts;
+                // document.getElementById("guessSoFar").innerHTML = currAttempts;
+
+
+                // document.getElementById('wins').innerHTML = "Wins: " + wins;
+                // document.getElementById('losses').innerHTML = "losses: " + losses;
+                // document.getElementById('guesses').innerHTML = "Guesses left: " + guesses;
+
+            }
+
         }
 
-        if (attempts === 0) {
-            losses++
 
 
 
+        // attempts = attempts - 1;
 
-            document.getElementById("wins").innerHTML = wins;
-            document.getElementById("loss").innerHTML = losses;
-            document.getElementById("guessLeft").innerHTML = attempts;
-            document.getElementById("guessSoFar").innerHTML = currAttempts;
-            // document.getElementById('wins').innerHTML = "Wins: " + wins;
-            // document.getElementById('losses').innerHTML = "losses: " + losses;
-            // document.getElementById('guesses').innerHTML = "Guesses left: " + guesses;
+        // document.getElementById("attempts").innerHTML = attempts;
+        // yourGuess.push(userChoice); //append user's choice to array yourGuess
 
-        }
+        // console.log("Your guesses so far: " + yourGuess); //test
+
+        // document.getElementById("your-guesses").innerHTML = yourGuess;
+
+        // console.log("Guesses Left: " + guessesLeft); //test
+
+        // noGuessesLeft();
 
     }
 
 
 
 
-    // attempts = attempts - 1;
 
-    // document.getElementById("attempts").innerHTML = attempts;
-    // yourGuess.push(userChoice); //append user's choice to array yourGuess
-
-    // console.log("Your guesses so far: " + yourGuess); //test
-
-    // document.getElementById("your-guesses").innerHTML = yourGuess;
-
-    // console.log("Guesses Left: " + guessesLeft); //test
-
-    // noGuessesLeft();
-
-};
-
-
-
-
-
-
+}
